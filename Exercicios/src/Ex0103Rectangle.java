8fc9ecf2f51559ae0ddd8a889c41ec57de943a68
Sergio de Magalhaes Nunes <sergio_mnunes@outlook.com>
2021-01-24 22:18:19 -0300

    Feitos exercicios 1.3 e 1.4

---
+++ Exercicios/src/Ex0103Rectangle.java
@@ -0,0 +1,42 @@
+public class Ex0103Rectangle {
+
+	private float length;
+	private float width;
+	
+	public Ex0103Rectangle() {
+		
+	}
+
+	public Ex0103Rectangle(float length, float width) {
+		this.length = length;
+		this.width = width;
+	}
+	
+	public float getLength() {
+		return length;
+	}
+	
+	public void setLength(float length) {
+		this.length = length;
+	}
+	
+	public float getWidth() {
+		return this.length;
+	}
+	
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	
+	public float getArea() {
+		return this.width * this.length;
+	}
+	
+	public float getPerimeter() {
+		return this.width*2 + this.length*2;
+	}
+	
+	public String toString() {
+		return "Rectangle[length=" + this.length + ",width=" + this.width + "]";
+	}
+}
