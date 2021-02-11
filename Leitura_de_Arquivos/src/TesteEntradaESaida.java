8e06b4781c0bad07760b1d132d9f026035da5752
Sergio de Magalhaes Nunes <sergio_mnunes@outlook.com>
2021-02-10 21:02:36 -0300

    Alteracao nos tipos dos objetos. FileInputStream para InputStream, InputStreamReader para Reader, FileOutputStream para OutputStream e OutputStreamWriter para Writer. Assim, o codigo fica mais generico para lidar com diferentes fluxos de IO.

--- Leitura_de_Arquivos/src/TesteEntradaESaida.java
+++ Leitura_de_Arquivos/src/TesteEntradaESaida.java
@@ -3,19 +3,23 @@ import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 
 public class TesteEntradaESaida {
 
 	public static void main (String[] args) throws IOException {
 		
-		FileInputStream fis = new FileInputStream("loren_ipsum.txt");
-		InputStreamReader isr = new InputStreamReader(fis);
+		InputStream fis = new FileInputStream("loren_ipsum.txt");
+		Reader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 
-		FileOutputStream fos = new FileOutputStream("loren_ipsum_de_saida.txt");
-		OutputStreamWriter osw = new OutputStreamWriter(fos);
+		OutputStream fos = new FileOutputStream("loren_ipsum_de_saida.txt");
+		Writer osw = new OutputStreamWriter(fos);
 		BufferedWriter bw = new BufferedWriter(osw);
 
 		// le uma linha e armazena na variavel "linha"
